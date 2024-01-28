f47ba535cdd00fae5acd095461e09cf1b9c42677
Nick Crews <nickcrews35@gmail.com>
2024-01-28 15:21:49 -0500

    Initial Commit

---
+++ BrainMacApp/BrainMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainMacAppApp.swift
+//  BrainMacApp
+//
+//  Created by Nick Crews on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainMacApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrainMacApp
+//
+//  Created by Nick Crews on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
